204f57105607c6a700397f9f3a7a778f10e259b0
Luis Angel Arvelo <laarvelo@icloud.com>
2023-10-03 03:52:29 +0000

    Setup gin server

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	db "github.com/Delavalom/RBD/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for our banking service.
+type Server struct {
+	store  *db.SQLStore
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server a setup routing.
+func NewServer(store *db.SQLStore) *Server {
+	server := &Server{
+		store: store,
+	}
+	router := gin.Default()
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
