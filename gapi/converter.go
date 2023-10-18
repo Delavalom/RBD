211a8e627676ad93d0ee0a5678f724f71c33534a
Luis Angel Arvelo <laarvelo@icloud.com>
2023-10-18 03:44:35 +0000

    Refactor create_user response struct

---
+++ gapi/converter.go
@@ -0,0 +1,17 @@
+package gapi
+
+import (
+	db "github.com/Delavalom/RBD/db/sqlc"
+	"github.com/Delavalom/RBD/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:         user.Username,
+		FullName:         user.FullName,
+		Email:            user.Email,
+		PasswordChangeAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:        timestamppb.New(user.CreatedAt),
+	}
+}
