386ba9c6636626ba28e21f21b3180941acd932b7
Luis Angel Arvelo <laarvelo@icloud.com>
2023-10-24 02:34:07 +0000

    Add server task options and update rpc_create_user handler

--- gapi/server.go
+++ gapi/server.go
@@ -7,26 +7,29 @@ import (
 	"github.com/Delavalom/RBD/pb"
 	"github.com/Delavalom/RBD/token"
 	"github.com/Delavalom/RBD/util"
+	"github.com/Delavalom/RBD/worker"
 )
 
 // Server serves gRPC requests for our banking service.
 type Server struct {
 	pb.UnimplementedRBDServer
-	config     util.Config
-	store      db.Store
-	tokenMaker token.Maker
+	config          util.Config
+	store           db.Store
+	tokenMaker      token.Maker
+	taskDistributor worker.TaskDistributor
 }
 
 // NewServer creates a new gRPC server.
-func NewServer(config util.Config, store db.Store) (*Server, error) {
+func NewServer(config util.Config, store db.Store, taskDistributor worker.TaskDistributor) (*Server, error) {
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create token maker: %w", err)
 	}
 	server := &Server{
-		config:     config,
-		store:      store,
-		tokenMaker: tokenMaker,
+		config:          config,
+		store:           store,
+		tokenMaker:      tokenMaker,
+		taskDistributor: taskDistributor,
 	}
 
 	return server, nil
