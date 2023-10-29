10e47a854321b63f4d50ec0852ebb70b81fb412c
Luis Angel Arvelo <laarvelo@icloud.com>
2023-10-29 00:42:08 +0000

    Autogenerated queries files

--- db/sqlc/user.sql.go
+++ db/sqlc/user.sql.go
@@ -75,9 +75,10 @@ SET
     hashed_password = COALESCE($1, hashed_password),
     password_changed_at = COALESCE($2, password_changed_at),
     full_name = COALESCE($3, full_name),
-    email = COALESCE($4, email)
+    email = COALESCE($4, email),
+    is_email_verified = COALESCE($5, is_email_verified)
 WHERE
-    username = $5
+    username = $6
 RETURNING username, hashed_password, full_name, email, password_changed_at, created_at, is_email_verified
 `
 
@@ -86,6 +87,7 @@ type UpdateUserParams struct {
 	PasswordChangedAt pgtype.Timestamptz `json:"password_changed_at"`
 	FullName          pgtype.Text        `json:"full_name"`
 	Email             pgtype.Text        `json:"email"`
+	IsEmailVerified   pgtype.Bool        `json:"is_email_verified"`
 	Username          string             `json:"username"`
 }
 
@@ -95,6 +97,7 @@ func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, e
 		arg.PasswordChangedAt,
 		arg.FullName,
 		arg.Email,
+		arg.IsEmailVerified,
 		arg.Username,
 	)
 	var i User
